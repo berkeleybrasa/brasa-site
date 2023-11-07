bd59968a5ca54fc2941264733abc6776af6490ee
berkeleybrasa <149536830+berkeleybrasa@users.noreply.github.com>
2023-11-07 12:51:10 -0800

    Update EmConstrucao.tsx

--- src/EmConstrucao/EmConstrucao.tsx
+++ src/EmConstrucao/EmConstrucao.tsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Navbar/>
     <div className='construction-container-master'>
-        <div className='construct-image-container'> <img id = 'construction-image' src='brasa-site/assets/under construction.png' alt = 'Under construction Icon. Source: Freepik'/></div>
+        <div className='construct-image-container'> <img id = 'construction-image' src='/brasa-site/assets/under-construction-ca695d62.png' alt = 'Under construction Icon. Source: Freepik'/></div>
         <div className='construction-text-container'>
             <p className='construction-title'>Erro: página em construção</p>
             <p className='construction-text'>Ainda não é possível acessar essa parte do site da Brasa Berkeley.
