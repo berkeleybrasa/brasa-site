e5375e12e308c1cf6ecb88ea20c245123011c1b3
berkeleybrasa <149536830+berkeleybrasa@users.noreply.github.com>
2023-11-07 01:27:48 -0800

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,19 @@
 function Navbar(){
     return <nav className="navbar navbar-expand-lg">
         <div className="container-fluid navbar-container">
-            <a className="navbar-brand" href="#"> <img src="/src/assets/brasa-logo.png" alt="Brasa Logo" width="66" height="66"/> </a>
+            <a className="navbar-brand" href="#"> <img src="/brasa-site/src/assets/brasa-logo.png" alt="Brasa Logo" width="66" height="66"/> </a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div className="navbar-nav">
-                <a className="active" id="navbar-title" aria-current="page" href="brasa-site/index.html">BRASA BERKELEY</a>
-                <a className="navbar-text" href="/src/EmConstrucao/index.html">Sobre</a>
-                <a className="navbar-text" href="/src/EmConstrucao/index.html">Eventos</a>
-                <a className="navbar-text" href="/src/EmConstrucao/index.html">Envolva-se</a>
-                <a className="navbar-text" href="/src/Membros/index.html">Membros</a>
-                <a className="navbar-text" href="/src/EmConstrucao/index.html">Recursos</a>
-                <a className="navbar-text" href="/src/EmConstrucao/index.html">Apoie</a>
+                <a className="active" id="navbar-title" aria-current="page" href="/brasa-site/index.html">BRASA BERKELEY</a>
+                <a className="navbar-text" href="/brasa-site/src/EmConstrucao/index.html">Sobre</a>
+                <a className="navbar-text" href="/brasa-site/src/EmConstrucao/index.html">Eventos</a>
+                <a className="navbar-text" href="/brasa-site/src/EmConstrucao/index.html">Envolva-se</a>
+                <a className="navbar-text" href="/brasa-site/src/Membros/index.html">Membros</a>
+                <a className="navbar-text" href="/brasa-site/src/EmConstrucao/index.html">Recursos</a>
+                <a className="navbar-text" href="/brasa-site/src/EmConstrucao/index.html">Apoie</a>
             </div>
             </div>
         </div>
