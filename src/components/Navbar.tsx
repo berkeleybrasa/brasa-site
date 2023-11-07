ca1cfdad3846f7f07b7d87fd0bb2b7d4bf32f612
berkeleybrasa <149536830+berkeleybrasa@users.noreply.github.com>
2023-11-07 11:04:33 -0800

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 function Navbar(){
     return <nav className="navbar navbar-expand-lg">
         <div className="container-fluid navbar-container">
-            <a className="navbar-brand" href="#"> <img src="/brasa-site/assets/brasa-logo.png" alt="Brasa Logo" width="66" height="66"/> </a>
+            <a className="navbar-brand" href="#"> <img src="/brasa-site/assets/brasa logo.png" alt="Brasa Logo" width="66" height="66"/> </a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
