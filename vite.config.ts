0a6e6fc05c2f6a3891912254927e073c384fc451
berkeleybrasa <149536830+berkeleybrasa@users.noreply.github.com>
2023-11-07 01:10:49 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ const outDir = resolve(__dirname, 'dist')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/brasa-site/src/",
+  base: "/brasa-site/",
   root,
   plugins: [react()],
   build: {
